c0cf156642c230c4ef3366146ba78d9cc1ee73d1
yuribertoli <yuribertoli@yahoo.it>
2022-03-14 20:27:06 +0100

    cors error fixed

--- database.php
+++ database.php
@@ -45,4 +45,8 @@
         ]
     ];
 
+    header("Access-Control-Allow-Origin","*");
+    header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
 ?>
--- server.php
+++ server.php
@@ -2,9 +2,10 @@
 
 require __DIR__ . '/database.php';
 
-/* header("Access-Control-Allow-Origin","*");
-header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE");
-header("Access-Control-Allow-Headers","Content-Type"); */
+header("Access-Control-Allow-Origin","*");
+header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
 header('Content-type: application/json');
 echo json_encode($database);
 
